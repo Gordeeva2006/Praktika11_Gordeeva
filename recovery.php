0b7b0fea2c4e7d992a384887441624dd5f4113cf
Gordeeva2006 <kgordeeva957@gmail.com>
2026-03-30 14:30:04 +0500

    Самостоятельно зашифровала данные при регистрации, восстановлении пароля, и комментировании записей. Гордеева

--- recovery.php
+++ recovery.php
@@ -16,6 +16,7 @@
 <html>
 	<head> 
 		<script src="https://code.jquery.com/jquery-1.8.3.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js"></script>
 		<meta charset="utf-8">
 		<title> Восстановление пароля </title>
 		
@@ -77,6 +78,25 @@
 			var button = document.getElementsByClassName("button")[0];
 			
 			errorWindow.style.display = "none";
+
+			const secretKey = "KrisGo";
+
+			function encryptAES(data, key) {
+				var keyHash = CryptoJS.MD5(key);
+				var keyBytes = CryptoJS.enc.Hex.parse(keyHash.toString());
+
+				var iv = CryptoJS.lib.WordArray.random(16);
+
+				var encrypted = CryptoJS.AES.encrypt(data, keyBytes, {
+					iv: iv,
+					mode: CryptoJS.mode.CBC,
+					padding: CryptoJS.pad.Pkcs7
+				});
+
+				var combined = iv.concat(encrypted.ciphertext);
+
+				return CryptoJS.enc.Base64.stringify(combined);
+			}
 		
 			function DisableError() {
 				errorWindow.style.display = "none";
@@ -91,8 +111,9 @@
 				loading.style.display = "block";
 				button.className = "button_diactive";
 				
+				var login = encryptAES(_login, secretKey);
 				var data = new FormData();
-				data.append("login", _login);
+				data.append("login", login);
 				
 				// AJAX запрос
 				$.ajax({
